cfe9cddb93601d2c268df1a140d8f01614cdaa51
Ashwin Kumar <ashwin027@gmail.com>
2020-05-18 14:49:53 -0400

    added ps script for db init

---
+++ SetupDb.ps1
@@ -0,0 +1,3 @@
+dotnet ef database update  --startup-project "ProductCatalog.Api"
+dotnet ef database update  --startup-project "Reviews.Api"
+dotnet ef database update  --startup-project "Registry.UI"
